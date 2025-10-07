8a87f1e2e1f13d7b5e30ca72beb60633ef27eaf9
Ezequiel <zikimuller017@gmail.com>
2025-10-07 20:26:39 -0300

    feat(nuxt-config): ssr ativado no projeto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
+  ssr: true,
   modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/eslint'],
   css: ['~/assets/css/main.css'],
   vite: {
