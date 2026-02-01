2d7c28fc3715a2f657b065b016ac43145aa79935
Ezequiel <zikimuller017@gmail.com>
2026-02-01 00:23:26 -0300

    project(format): eslint e prettier - configuracoes de formatacao do codigo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-config-prettier'
 
 export default withNuxt({
+  extends: [prettier],
+
   rules: {
     'vue/html-self-closing': [
       'error',
