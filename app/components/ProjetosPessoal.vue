1944377195587a5b84bd15804ec9ba5db3c46891
Ezequiel <zikimuller017@gmail.com>
2026-02-01 01:01:43 -0300

    style(sessao-projetos): card centralizado

--- app/components/ProjetosPessoal.vue
+++ app/components/ProjetosPessoal.vue
@@ -3,9 +3,10 @@
     <h1 class="text-[22px] md:text-2xl font-medium text-center">Projetos</h1>
   </div>
 
-  <div
+  <!-- <div
     class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center"
-  >
+  > -->
+  <div class="justify-center grid">
     <UiCardProjeto
       v-for="projeto in projetos"
       :key="projeto.id"
